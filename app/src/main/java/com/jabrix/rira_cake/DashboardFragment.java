ae0e0113c6372e53dcb2559d86681ee65932ee6d
Rizki354 <rizkinasrulloh88@gmail.com>
2024-11-20 09:24:20 +0700

    fix bug

--- app/src/main/java/com/jabrix/rira_cake/DashboardFragment.java
+++ app/src/main/java/com/jabrix/rira_cake/DashboardFragment.java
@@ -4,38 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.navigation.NavigationView;
+import com.jabrix.rira_cake.R;
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private ImageButton buttonDrawerToogle;
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DashboardFragment newInstance(String param1, String param2) {
         DashboardFragment fragment = new DashboardFragment();
         Bundle args = new Bundle();
@@ -46,7 +36,7 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -54,10 +44,13 @@ public class DashboardFragment extends Fragment {
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return view;
     }
+
+
 }
--- app/src/main/java/com/jabrix/rira_cake/MainActivity.java
+++ app/src/main/java/com/jabrix/rira_cake/MainActivity.java
@@ -2,28 +2,23 @@ package com.jabrix.rira_cake;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     private static final String KEY_EMAIL = "email";
     private static final String KEY_USERNAME = "username";
 
-    private DrawerLayout drawer;
-    private NavigationView navigationView;
-    private ImageButton buttonDrawerToogle;
+
+
     private BottomNavigationView bottomNavigationView;
     private TextView textView;
     private SharedPreferences sharedPreferences;
@@ -33,70 +28,12 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     OverviewFragment thirdFragment = new OverviewFragment();
     SettingFragment fourFragment = new SettingFragment();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initViews();
-        handleIntentExtras();
-    }
-
-    private void initViews() {
-        drawer = findViewById(R.id.drawer_layout);
-        edtUsername = findViewById(R.id.edt_username);
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.dashboard);
-        buttonDrawerToogle = findViewById(R.id.btn_drawer);
-        navigationView = findViewById(R.id.drawer_menu);
-        textView = findViewById(R.id.txt_halo);
-
-        buttonDrawerToogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawer.open();
-            }
-        });
-
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item){
-
-                int itemId = item.getItemId();
-
-                if (itemId == R.id.profile) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
-                } else if (itemId == R.id.contact) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
-                } else if (itemId == R.id.settings) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
-                }
-                drawer.close();
-
-                return false;
-            }
-        });
-    }
-
-    private void handleIntentExtras() {
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            if (extras.containsKey(KEY_USERNAME)) {
-                String username = extras.getString(KEY_USERNAME);
-                textView.setText("Haloo "+username);
-            }
-        }
-    }
-
-
-
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         int itemId = item.getItemId();
+
 
         if (itemId == R.id.dashboard) {
             getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
@@ -114,4 +51,35 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
         return false;
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+
+        edtUsername = findViewById(R.id.edt_username);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
